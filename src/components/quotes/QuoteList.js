17f545b17ddfbc4d48e9fa87828f41eff0111829
Robert Muñoz Marzo <robertmunozmarzo@Roberts-MacBook-Air.local>
2022-06-28 17:41:52 +0200

    Add real fetched/submitted data to app

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -22,11 +22,13 @@ const QuoteList = (props) => {
   const queryParams = new URLSearchParams(location.search);
 
   const isSortingAscending = queryParams.get("sort") === "asc";
-
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
-    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${isSortingAscending ? "desc" : "asc"}`,
+    });
   };
 
   return (
